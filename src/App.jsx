aaee18432b17fa394f55a621281ec1b660c9cd69
Criss <Jhonataansantacruz@gmail.com>
2024-07-31 21:48:29 -0400

    se creo la lista de criptomonedas, se agrego axios y se creo el archivo Css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const API_URL = import.meta.env.VITE_API_URL
+  const [criptos, setCriptos] = useState()
+
+  useEffect(() => {
+    axios.get(`${API_URL}assets`)
+      .then((data) => {
+        console.log(data.data)
+        setCriptos(data.data.data)
+      })
+      .catch(() => {
+        console.error("La peticion falló")
+      })
+  }, [])
+
+  if (!criptos) return <span>Cargando...</span>
 
   return (
     <>
-    <h1>Hola mundo</h1>
+      <h1>Lista de criptomonedas</h1>
+      <ol>
+        {criptos.map(({ name, priceUsd, id}) => (
+          <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+        ))}
+      </ol>
     </>
   )
 }
