905a4793a515f5121c40426742f6688f5d8090ef
Criss <Jhonataansantacruz@gmail.com>
2025-03-21 00:38:47 -0400

    Cambiando el nombre

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     return (
         <>
             <div className="home-container">
-                <h1 className="title">Hola, bienvenido a EdMarket</h1>
+                <h1 className="title">Hola, bienvenido a CriptoMarket</h1>
                 <p className="text">Conoce las 100 criptos mas utilizadas</p>
                 <Link to="/criptomonedas" className="btn">Ver criptomonedas</Link>
             </div>
